1c8b66d877d1009ad871095833ef1957f419780e
trustonlyyou <trustonlyyou@naver.com>
2022-03-05 04:46:38 +0900

    init

---
+++ src/test/java/com/rest/board/boardrest/BoardRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rest.board.boardrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
